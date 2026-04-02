45e6269f8b10c233965220677da09af81e6821a1
Mowaah <mohamed.bahaa5300@gmail.com>
2026-04-01 21:51:57 -0400

    important links

--- src/components/website/TripDetailPage/TripImportantLinks/TripImportantLinks.tsx
+++ src/components/website/TripDetailPage/TripImportantLinks/TripImportantLinks.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { Trip } from "@/types";
 import styles from "./TripImportantLinks.module.scss";
 
@@ -11,10 +12,7 @@ export default function TripImportantLinks({ trip }: Props) {
   return (
     <section id="more-adventures" className={styles.section}>
       <div className={styles.banner}>
-        <svg width="18" height="18" viewBox="0 0 24 24" fill="none">
-          <circle cx="12" cy="12" r="10" fill="#EF4444" opacity="0.15"/>
-          <path d="M12 8v4M12 16h.01" stroke="#EF4444" strokeWidth="2" strokeLinecap="round"/>
-        </svg>
+        <Image src="/images/caution.svg" alt="Important" width={20} height={20} />
         <span>Please make sure to review the following links</span>
       </div>
 
@@ -27,9 +25,7 @@ export default function TripImportantLinks({ trip }: Props) {
         {links.map((link, i) => (
           <Link key={i} href={link.href} className={styles.pill}>
             {link.label}
-            <svg width="14" height="14" viewBox="0 0 24 24" fill="none">
-              <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6M15 3h6v6M10 14L21 3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
+            <Image src="/images/arrows/arrow-diagonal.svg" alt="arrow" width={30} height={30} className={styles.arrowIcon} />
           </Link>
         ))}
       </div>
